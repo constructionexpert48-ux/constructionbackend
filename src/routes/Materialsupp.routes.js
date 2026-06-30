a50bc6447637df726a4c311df7849fa5320fdb64
mac <mac@macs-MacBook-Air.local>
2026-07-01 01:17:49 +0530

    this is

--- src/routes/Materialsupp.routes.js
+++ src/routes/Materialsupp.routes.js
@@ -1,9 +1,10 @@
 const express = require ("express")
 const  router = express.Router();
+const upload = require('../middleware/upload.js');
 const {createMaterialsupp , getAllMaterial} = require("../controllers/Materialsupp.controller");
 const auth = require('../middleware/auth');
 
-router.post('/create' , createMaterialsupp);
+router.post('/create', upload, createMaterialsupp);
 
 router.get('/' , auth , getAllMaterial);
 
--- src/routes/mechanical.routes.js
+++ src/routes/mechanical.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.js');
+const upload = require('../middleware/upload.js');
 const {createMechanical , getAllMechanical} = require('../controllers/mechanical.controller');
 
-router.post('/create', createMechanical);
+router.post('/create',  upload, createMechanical);
 
 router.get('/', auth , getAllMechanical);
 
